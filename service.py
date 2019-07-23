b3c8baa198ded7a8dc26bf934bcba0d5a4a32041
Sumithran <sumithran@outlook.in>
2019-07-23 17:57:42 +0000

    added service

---
+++ service.py
@@ -0,0 +1,6 @@
+class ToDoService:
+    def __init__(self):
+        self.model = ToDoModel()
+        
+    def create(self, params):
+        self.model.create(params["text"], params["Description"])
